f2576d8905f0e2ead671bece2962d6c1621aeb0e
experimentsym3 <gezginailesg@gmail.com>
2025-06-23 01:09:31 +0000

    Deploying to gh-pages from @ experimentsym3/experimentsym3.github.io@81813e3a9f730917a41de2e258f233448ac5c188 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -56,9 +56,9 @@ ninja.data = [{
           description: "Building and training a multi-layer perceptron for next-word prediction on a 250-word vocabulary using only NumPy",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
-            },},{id: "projects-gaussian-mixture-modeling-with-expectation-maximization",
-          title: 'Gaussian Mixture Modeling with Expectation-Maximization',
-          description: "Step-by-step implementation of the expectation-maximization algorithm for gaussian mixture models with dynamic clustering visualizations",
+            },},{id: "projects-expectation-maximization-algorithm-for-gmm-from-scratch",
+          title: 'Expectation-Maximization Algorithm for GMM from Scratch',
+          description: "Step-by-step implementation of the expectation-maximization (EM) algorithm for gaussian mixture models (GMMs)",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
             },},{id: "projects-project-5",
